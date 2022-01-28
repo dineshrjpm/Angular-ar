38212f4850770e7127c5e60025ea7c07c020f579
RAJ Dinesh <dinesh.raj@rntbci.com>
2022-01-28 19:12:40 +0530

    add files

---
+++ src/assets/js/main.js
@@ -0,0 +1,33 @@
+
+const THREE = window.MINDAR.IMAGE.THREE;
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const start = async() => {
+    mockWithVideo('assets/mock-videos/course-banner1.mp4');
+    //mockWithImage('assets/mock-videos/course-banner1.png');
+    // initialize MindAR 
+    const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+      container: document.body,
+      imageTargetSrc: 'assets/TargetMind/course-banner.mind',
+    });
+    const {renderer, scene, camera} = mindarThree;
+
+    // create AR object
+    const geometry = new THREE.PlaneGeometry(1, 1);
+    const material = new THREE.MeshBasicMaterial({color: 0x00ffff, transparent: true, opacity: 0.5});
+    const plane = new THREE.Mesh(geometry, material);
+
+    // create anchor
+    const anchor = mindarThree.addAnchor(0);
+    anchor.group.add(plane);
+
+    // start AR
+    await mindarThree.start();
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera);
+    });
+  }
+  start();
+});
